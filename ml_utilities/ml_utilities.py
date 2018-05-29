a77eed7e80638bde5484c72b00189ec2fe157a1a
Truoc Pham <truoc.phamkhac@asnet.com.vn>
2018-05-29 17:21:37 +0700

    Fix wrong on missing data statistics helper.

--- ml_utilities/ml_utilities.py
+++ ml_utilities/ml_utilities.py
@@ -32,8 +32,7 @@ def missing_data_stats(df):
         # display missing data
         missing_data
     """
-    nan_df = (df.isnull().sum() / len(df)) * 100
-    nan_df = nan_df.drop(nan_df[nan_df == 0].index).sort_values(ascending=False)
+    nan_df = ((df.isnull().sum() / df.isnull().count()) * 100).sort_values(ascending=False)
     missing_data = pd.DataFrame({'Missing Ratio': nan_df})
 
     return missing_data
