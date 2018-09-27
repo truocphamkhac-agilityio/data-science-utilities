c35fcf0491fa631238cdaa9ba6bec1727090e4ae
Thong Nguyen <thong.nguyenhuu@asnet.com.vn>
2018-09-27 14:32:36 +0700

    Change version to 1.0.2

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://gitlab.asoft-python.com/bgh/data-science/ml-utilities',
-    version='1.0.1',
+    version='1.0.2',
     zip_safe=False
 )
